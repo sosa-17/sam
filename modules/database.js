8d0f73faecfd8c21032d5089ea773714e721bd64
sosa-20 <sosa96aleman@gmail.com>
2020-12-19 14:00:38 -0600

    refactor: "Se corrigio conexion con mongo:

--- modules/database.js
+++ modules/database.js
@@ -1,20 +1,27 @@
-'use strict';
+"use strict";
 
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
 
-var servidor = 'localhost:27017';
-var db = 'sam';
+var servidor = "localhost:27017";
+var db = "sam";
 
-class Database{
-    constructor(){
-        //Promesas
-        mongoose.connect(`mongodb://${servidor}/${db}`, { useNewUrlParser: true })
-        .then(()=>{
-            console.log('Se conecto a mongo');
-        }).catch((error)=>{
-            console.log(error);
-        });
-    }
+class Database {
+  constructor() {
+    //Promesas
+    mongoose
+      .connect(`${process.env.MONGO_DB_URI}`, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
+      .then(() => {
+        console.log("Se conecto a mongo");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 }
 
 module.exports = new Database();
