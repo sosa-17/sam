99fcc0e43ab606ac68c98576f7aed5a42b9f7b50
sosa-20 <sosa96aleman@gmail.com>
2020-12-31 11:38:16 -0600

    feat: conectar con mongo atlas

--- modules/database.js
+++ modules/database.js
@@ -1,20 +1,24 @@
-'use strict';
+"use strict";
 
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 
-var servidor = 'localhost:27017';
-var db = 'sam';
-
-class Database{
-    constructor(){
-        //Promesas
-        mongoose.connect(`mongodb://${servidor}/${db}`, { useNewUrlParser: true })
-        .then(()=>{
-            console.log('Se conecto a mongo');
-        }).catch((error)=>{
-            console.log(error);
-        });
-    }
+var servidor = "localhost:27017";
+var db = "sam";
+class Database {
+  constructor() {
+    //Promesas
+    mongoose
+      .connect("mongodb+srv://sam:asd.456@cluster0.9atxk.mongodb.net/test", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
+      .then(() => {
+        console.log("Se conecto a mongo");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 }
 
 module.exports = new Database();
