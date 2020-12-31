52997b762b82e8a7ddb2a860cc20572585ee9623
sosa-20 <sosa96aleman@gmail.com>
2020-12-31 13:31:13 -0600

    deploy

--- index.js
+++ index.js
@@ -1,46 +1,45 @@
-
-var express = require("express"),session = require('express-session');
-var multer  =   require('multer');  
+var express = require("express"),
+  session = require("express-session");
+var multer = require("multer");
 var bodyParser = require("body-parser");
 var database = require("./modules/database");
 
-var usuariosRouter = require('./routers/loggin-routers');
-var categoriasRouter = require('./routers/categorias-routers');
-var archivosRouter = require('./routers/archivos-routers');
-var entradasRouter = require('./routers/entradas-routers');
-var comentariosRouter = require('./routers/comentarios-routers');
+var usuariosRouter = require("./routers/loggin-routers");
+var categoriasRouter = require("./routers/categorias-routers");
+var archivosRouter = require("./routers/archivos-routers");
+var entradasRouter = require("./routers/entradas-routers");
+var comentariosRouter = require("./routers/comentarios-routers");
 
 var app = express();
 
 //subir archivo al servidor
-var storage =   multer.diskStorage({  
-    destination: function (req, file, callback) {  
-      callback(null, './www/uploads');  
-    },  
-    filename: function (req, file, callback) {  
-      callback(null, file.originalname);  
-    }  
-  });  
-  var upload = multer({ storage : storage}).single('myfile');
+var storage = multer.diskStorage({
+  destination: function (req, file, callback) {
+    callback(null, "./www/uploads");
+  },
+  filename: function (req, file, callback) {
+    callback(null, file.originalname);
+  },
+});
+var upload = multer({ storage: storage }).single("myfile");
 ////
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //subir archivo al servidor
-app.post('/uploadjavatpoint',function(req,res){  
-    upload(req,res,function(err) {  
-        if(err) {  
-            return res.end("error al subir archivo.");  
-        }  
-        res.end("agregado con exito!");  
-    });  
+app.post("/uploadjavatpoint", function (req, res) {
+  upload(req, res, function (err) {
+    if (err) {
+      return res.end("error al subir archivo.");
+    }
+    res.end("agregado con exito!");
+  });
 });
-
 
 ///////
 
-app.use('/categorias', categoriasRouter);
+app.use("/categorias", categoriasRouter);
 app.use("/usuarios", usuariosRouter);
 app.use("/archivos", archivosRouter);
 app.use("/entradas", entradasRouter);
@@ -48,6 +47,6 @@ app.use("/comentarios", comentariosRouter);
 
 app.use(express.static("www"));
 
-app.listen(3336,function(){
-    console.log("Servidor en linea");
+app.listen(process.env.PORT || 3336, function () {
+  console.log("Servidor en linea");
 });
